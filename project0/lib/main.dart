c197cfc40df3e52dcf0517322de87ca02bece568
XcEland <mkoma.xyz@gmail.com>
2024-07-01 14:07:37 +0200

    container and sizedbox

---
+++ project0/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:project0/widgets/container_sized.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.dark, primaryColor: Colors.deepPurple),
+      home: Container_Sized(),
+    );
+  }
+}
