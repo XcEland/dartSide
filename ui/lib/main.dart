197a6b663ac8b87bfa95cfb806fc6b3387eaaed2
XcEland <mkoma.xyz@gmail.com>
2024-07-05 11:58:03 +0200

    cool botton nav

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -6,8 +6,14 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int myIndex = 0;
   @override
   Widget build(BuildContext context) {
     const String appTitle = 'Flutter layout demo';
@@ -22,7 +28,41 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text(appTitle),
           ),
-          body: RowsCols(),
+          // body: RowsCols(),
+          bottomNavigationBar: BottomNavigationBar(
+            selectedItemColor: Colors.orange,
+            unselectedItemColor: Colors.grey,
+            // backgroundColor: Colors.white,
+
+            onTap: (index) {
+              setState(() {
+                myIndex = index;
+              });
+            },
+            currentIndex: myIndex,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+                backgroundColor: Colors.yellow,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'Bookings',
+                backgroundColor: Colors.blue,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.contact_phone),
+                label: 'Contact',
+                backgroundColor: Colors.white,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.receipt),
+                label: 'Reports',
+                backgroundColor: Colors.green,
+              ),
+            ],
+          ),
         ));
   }
 }
