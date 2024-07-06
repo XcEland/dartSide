cdb9c75ee5ad66354a19f7c87f28c5b35cfceba9
XcEland <mkoma.xyz@gmail.com>
2024-07-06 13:24:48 +0200

    const and final

---
+++ dartguy/basics/const_final.dart
@@ -0,0 +1,9 @@
+//A final variable can be set only once; a const variable is a compile-time constant. (Const variables are implicitly final.)
+final name = 'Bob';
+final String nickname = 'Bobby';
+
+const baz = [];
+
+void main() {
+  // baz = [1, 2, 3];
+}
