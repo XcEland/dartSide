f652e9b37b740c8a239724817e7565eb0ce1c3d9
XcEland <mkoma.xyz@gmail.com>
2024-07-06 17:44:13 +0200

    break

---
+++ dartguy/basics/loops.dart
@@ -0,0 +1,14 @@
+//for loop
+//while and do while loops
+// break and continue
+
+// Branching, like if and switch
+// Exceptions, like try, catch, and throw
+
+var message = StringBuffer('Dart is fun');
+
+void main() {
+  for (var i = 0; i < 5; i++) {
+    message.write('!');
+  }
+}
