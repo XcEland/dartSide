5fd9a9f70a17572d3021a854ea7733512de98b2a
XcEland <mkoma.xyz@gmail.com>
2024-07-06 15:27:25 +0200

    functions

---
+++ dartguy/basics/function.dart
@@ -0,0 +1,15 @@
+// bool isNoble(int atomicNumber) {
+//   return _nobleGases[atomicNumber] != null;
+// }
+
+// bool isNoblle(int atomicNum) => _noblleGases[atomicNum] != null;
+
+var List = [1, 2, 3, 4];
+
+void printElement(int element) {
+  print(element);
+}
+
+void main() {
+  List.forEach(printElement);
+}
