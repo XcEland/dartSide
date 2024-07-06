c42c153561fddbcfd79afe99a8a5431627b00d21
XcEland <mkoma.xyz@gmail.com>
2024-07-06 12:51:24 +0200

    variables

---
+++ dartguy/basics/variables.dart
@@ -0,0 +1,8 @@
+var name = 'Voyager 1';
+var year = 1997;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['saturn'],
+  'url': '//path/to/saturn.jpg'
+};
