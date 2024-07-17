d900a2d07efa27bd17f408c9318e5f634903af87
XcEland <mkoma.xyz@gmail.com>
2024-07-17 13:18:13 +0200

    paymentmethods

--- delivery/lib/widgets/payment.dart
+++ delivery/lib/widgets/payment.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 class PaymentWidget extends StatelessWidget {
   const PaymentWidget({super.key});
@@ -7,10 +7,36 @@ class PaymentWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Payment'),
+        title: const Text('Taxify delivery'),
+        centerTitle: true,
+        backgroundColor: const Color.fromRGBO(22, 142, 247, 1),
+        foregroundColor: Colors.white,
       ),
-      body: Container(
-        child: Text('Hello'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Payment method',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 12),
+              const Text(
+                'Select the payment method you want',
+                style: TextStyle(fontSize: 12),
+              ),
+              const SizedBox(height: 24),
+              PaymentMethodsScreen(),
+              const SizedBox(height: 24,),
+              
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -18,7 +44,7 @@ class PaymentWidget extends StatelessWidget {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => PaymentWidget()),
+              MaterialPageRoute(builder: (context) => const PaymentWidget()),
             );
           },
           style: ElevatedButton.styleFrom(
@@ -38,6 +64,75 @@ class PaymentWidget extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class PaymentMethodsScreen extends StatefulWidget {
+  const PaymentMethodsScreen({super.key});
+
+  @override
+  _PaymentMethodsScreenState createState() => _PaymentMethodsScreenState();
+}
+
+class _PaymentMethodsScreenState extends State<PaymentMethodsScreen> {
+  String? _selectedPaymentMethod;
+
+  // Payment methods data
+  final List<Map<String, dynamic>> _paymentMethods = [
+    {
+      'image': Icons.payment,
+      'title': 'PayPal',
+    },
+    {
+      'image': Icons.apple,
+      'title': 'Apple Pay',
+    },
+    {
+      'image': Icons.credit_card,
+      'title': '**** **** **** 1234',
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: _paymentMethods.map((method) {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 16.0),
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(12),
+            // border: Border.all(
+            //   color: const Color.fromRGBO(22, 142, 247, 1),
+            // ),
+          ),
+          child: Row(
+            children: [
+              Icon(method['image']),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Text(method['title'],
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),),
+              ),
+              Radio<String>(
+                value: method['title'],
+                groupValue: _selectedPaymentMethod,
+                activeColor: const Color.fromRGBO(22, 142, 247, 1),
+                onChanged: (String? value) {
+                  setState(() {
+                    _selectedPaymentMethod = value;
+                  });
+                },
+              ),
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
